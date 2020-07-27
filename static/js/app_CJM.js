47b95a28f9a9c2d0b32f92c42768890aa0f5355f
unknown <cmikus6@gmail.com>
2020-07-26 21:23:46 -0400

    Adding the gauge bonus after original submission

--- static/js/app_CJM.js
+++ static/js/app_CJM.js
@@ -7,12 +7,12 @@ function getInfo(id) {
         Object.entries(result).forEach((key) => {
         demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
         });
+        gaugeBonus(result.wfreq);
     });
 }
 
 function getPlot(id) {
     d3.json("samples.json").then((data)=> {
-    var wfreq = data.metadata.map(d => d.wfreq)
     var samples = data.samples.filter(s => s.id.toString() === id)[0];
     var samplevalues = samples.sample_values.slice(0, 10).reverse();
     var OTU_top = (samples.otu_ids.slice(0, 10)).reverse();
@@ -61,39 +61,96 @@ var layout_b = {
 };
 var data_b = [trace_b];
 Plotly.newPlot("bubble", data_b, layout_b);
-
-
 });
 }
 
 // The guage chart
-// var data_g = [{
-//     domain: { x: [0, 1], y: [0, 1] },
-//     value: parseFloat(wfreq),
-//     title: { text: `Weekly Washing Frequency ` },
-//     type: "indicator",
-//     mode: "gauge+number",
-//     gauge: { axis: { range: [null, 9] },
-//         steps: [
-//         { range: [0, 2], color: "#A9D0F5" },
-//         { range: [2, 4], color: "#2E64FE" },
-//         { range: [4, 6], color: "#0000FF" },
-//         { range: [6, 8], color: "#0404B4" },
-//         { range: [8, 9], color: "#0B0B3B" },
-//         ]}
-// }];
-// console.log(data_g);
-// var layout_g = {
-//     width: 800,
-//     height: 650,
-//     margin: { t: 20, b: 40, l:100, r:100 }
-// };
-// Plotly.newPlot("gauge", data_g, layout_g);
+function gaugeBonus(wfreq) {
+    var level = parseFloat(wfreq)*20;
+    var degrees = 180 - level;
+    var radius = 0.5;
+    var radians = (degrees * Math.PI) / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
 
-function optionChanged(newid) {
-    getInfo(newid);
-    getPlot(newid); 
+    var path = "M -.0 -0.05 L .0 0.05 L ";
+    var pathx = String(x);
+    var space = " ";
+    var pathy = String(y);
+    var pathEnd = " Z";
+    var path = path.concat(pathx, space, pathy, pathEnd);
+
+    var data = [
+        {
+          type: "scatter",
+          x: [0],
+          y: [0],
+          marker: { size: 12, color: "850000" },
+          showlegend: false,
+          name: "Freq",
+          text: level,
+          hoverinfo: "text+name"
+        },
+        {
+          values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+          rotation: 90,
+          text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+          textinfo: "text",
+          textposition: "inside",
+          marker: {
+            colors: [
+              "rgba(0, 105, 11, .5)",
+              "rgba(10, 120, 22, .5)",
+              "rgba(14, 127, 0, .5)",
+              "rgba(110, 154, 22, .5)",
+              "rgba(170, 202, 42, .5)",
+              "rgba(202, 209, 95, .5)",
+              "rgba(210, 206, 145, .5)",
+              "rgba(232, 226, 202, .5)",
+              "rgba(240, 230, 215, .5)",
+              "rgba(255, 255, 255, 0)"
+            ]
+          },
+          labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+          hoverinfo: "label",
+          hole: 0.5,
+          type: "pie",
+          showlegend: false
+        }
+    ];
+    var layout = {
+        shapes: [
+          {
+            type: "path",
+            path: path,
+            fillcolor: "850000",
+            line: {
+              color: "850000"
+            }
+          }
+        ],
+        title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+        height: 500,
+        width: 500,
+        xaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1, 1]
+        },
+        yaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1, 1]
+        }
+      };
+    var GAUGE = document.getElementById("gauge");
+    console.log("Is this running");
+    console.log
+    Plotly.newPlot(GAUGE, data, layout);
 }
+
 function init() {
     var dropdown = d3.select("#selDataset");
     d3.json("samples.json").then((data)=> {
@@ -103,6 +160,10 @@ function init() {
     getInfo(data.names[0]);
     getPlot(data.names[0]);
     });
-    
 }
+function optionChanged(newid) {
+    getInfo(newid);
+    getPlot(newid);
+}
+
 init();
